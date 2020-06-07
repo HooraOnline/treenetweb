3bcb2b52a00b24b0326b2a7bbaf1ce6e63bcb2bb
saeed <hooraonline@gmail.com>
2020-06-07 09:02:55 +0430

    register user props multy language

--- src/language/translate.js
+++ src/language/translate.js
@@ -3,8 +3,15 @@
  import {ar} from "./ar"
  import {ch} from "./ch"
  import {tu} from "./tu"
+ import {saveCookie} from "../utils";
  let dictionary =Object.assign(fa,en,ar,ch,tu);
- global.slanguage=global.slanguage
+
+ //temp**************************
+ global.slanguage = 'en';
+ global.isRtl = false;
+ saveCookie('lng', {index:0,title: 'English',key:'en',rtl:false});
+ //temp************************
+
 const translate=(keyword)=> {
      let key=`${global.slanguage}_${keyword}`;
      return  dictionary[`${key}`] || keyword;
